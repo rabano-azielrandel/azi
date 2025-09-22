7649ab2c32ec6e14c96a696ec4e570dc9e064b98
Aziel Randel Rabano <imazielr@gmail.com>
2025-09-22 22:52:30 +0800

    WIP in project cards top part

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,11 +9,31 @@ const Projects = () => {
     >
       <div className="relative z-10 w-full max-w-[1360px] h-full mx-auto p-4 gap-2 flex flex-col justify-center items-center">
         <div className="w-full h-full flex gap-4">
-          <div className="w-full rounded-xl p-4 bg-white/3 border-1 border-theme1-secondary ">
+          <div className="w-full rounded-xl p-4 bg-white/3 border-1 border-theme1-secondary">
             <SplitCarousel />
           </div>
-          <div className="w-full rounded-xl p-4 bg-white/3">top mid</div>
-          <div className="w-full rounded-xl p-4 bg-white/3">top right</div>
+          <div className="w-full rounded-xl p-4 bg-white/3 border-1 border-theme1-secondary">
+            <div className="w-full h-4/5"> a </div>
+            <div className="w-full h-1/5">
+              <h1 className="text-lg font-bold text-white leading-tight">
+                Resort Management
+              </h1>
+              <p className="text-sm text-gray-200 leading-snug">
+                Streamline reservations, billing, and staff managemnt.
+              </p>
+            </div>
+          </div>
+          <div className="w-full rounded-xl p-4 bg-white/3 border-1 border-theme1-secondary">
+            <div className="w-full h-4/5"> a </div>
+            <div className="w-full h-1/5">
+              <h1 className="text-lg font-bold text-white leading-tight">
+                Truck Scale
+              </h1>
+              <p className="text-sm text-gray-200 leading-snug">
+                Weigh the trucks.
+              </p>
+            </div>
+          </div>
         </div>
         <div className="w-full h-full flex gap-4">
           <div className="w-[67%] rounded-xl p-4 bg-white/3">bot left</div>
