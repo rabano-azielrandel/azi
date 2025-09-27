94db51b03d3847fc946288f09588412b5516f672
Aziel Randel Rabano <imazielr@gmail.com>
2025-09-28 01:36:59 +0800

    WIP in projects

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -8,7 +8,7 @@ const Experience = () => {
       className="relative mt-8 w-full px-4 h-[700px] overflow-hidden"
     >
       <div className="relative z-10 w-full max-w-[1360px] h-full mx-auto p-4 gap-12 flex flex-col justify-center items-center">
-        <p>EXPERIENCE</p>
+        <p className="font-oswald text-5xl text-white ">EXPERIENCE</p>
 
         <Timeline />
       </div>
