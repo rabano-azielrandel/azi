24b047e4a58ef09ac2b519a525ab5077c3d73572
Aziel Randel Rabano <imazielr@gmail.com>
2025-09-28 22:39:15 +0800

    Remove overflow in experience child div

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -8,7 +8,7 @@ const Experience = () => {
       id="experience"
       className="relative mt-14 w-full px-4 h-[1000px] overflow-hidden"
     >
-      <div className="relative z-10 w-full max-w-[1360px] h-full mx-auto p-4 gap-12 flex flex-col justify-center items-center overflow-hidden">
+      <div className="relative z-10 w-full max-w-[1360px] h-full mx-auto p-4 gap-12 flex flex-col justify-center items-center">
         <Meteors number={30} />
         <p className="font-oswald text-5xl text-theme1-secondary ">
           EXPERIENCE
