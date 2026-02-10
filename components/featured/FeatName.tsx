531b8ea2cf9dd766c6d27e842bed9269da6362cb
Aziel Randel Rabano <imazielr@gmail.com>
2026-02-10 13:50:28 +0800

    update featured name to use uppercase

--- components/featured/FeatName.tsx
+++ components/featured/FeatName.tsx
@@ -8,7 +8,9 @@ export default function FeatName({ name }: FeatNameProps) {
   return (
     <div className="flex flex-col w-full h-64">
       <div className="flex flex-col w-full h-2/3 mt-20 px-20 gap-4 justify-center text-theme1-secondary">
-        <h2 className="text-6xl font-semibold tracking-wider">{name}</h2>
+        <h2 className="text-6xl font-semibold tracking-wider">
+          {name.toUpperCase()}
+        </h2>
         <p className="text-xl font-medium tracking-wide">
           {name == "Thesis Project" ? "MOBILE APP" : "WEB APP"}
         </p>
